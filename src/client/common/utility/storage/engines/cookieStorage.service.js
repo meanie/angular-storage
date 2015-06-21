bd31aacb724152a165a034fc83d3ca1b7468f4b6
Adam Buczynski <adambuczynski@gmail.com>
2015-06-21 18:01:50 +1200

    Refactoring into separate storage engines

---
+++ src/client/common/utility/storage/engines/cookieStorage.service.js
@@ -0,0 +1,72 @@
+
+/**
+ * Module definition and dependencies
+ */
+angular.module('Utility.Storage.Engines.CookieStorage.Service', [])
+
+/**
+ * Cookie storage engine service
+ */
+.factory('CookieStorage', function CookieStorage($injector) {
+
+	//Get cookies service
+	var $cookies;
+	if ($injector.has('$cookies')) {
+		$cookies = $injector.get('$cookies');
+	}
+	else {
+		console.warn('Cookie storage requires the `ngCookies` module as a dependency.');
+	}
+
+	/**
+	 * Storage engine interface
+	 */
+	return {
+
+    /**
+     * Check if supported
+     */
+    isSupported: function() {
+			return !!$cookies;
+    },
+
+    /**
+     * Set an item
+     */
+    set: function(key, value) {
+      $cookies.put(key, value);
+    },
+
+    /**
+     * Get an item
+     */
+    get: function(key) {
+      return $cookies.get(key);
+    },
+
+    /**
+     * Remove an item
+     */
+    remove: function(key) {
+      $cookies.remove(key);
+    },
+
+		/**
+		 * Clear items
+		 */
+		clear: function(prefix) {
+
+			//Get all the cookies and corresponding keys
+			var prefixRegex = prefix ? new RegExp('^' + prefix) : null,
+					cookies = $cookies.getAll(),
+					keys = cookies ? Object.keys(cookies) : [];
+
+			//Loop keys
+			for (var k = 0; k < keys.length; k++) {
+				if (!prefix || prefixRegex.test(keys[k])) {
+					$cookies.remove(keys[k]);
+				}
+			}
+		}
+  };
+});
---
+++ src/client/common/utility/storage/engines/localStorage.service.js
@@ -0,0 +1,74 @@
+
+/**
+ * Module definition and dependencies
+ */
+angular.module('Utility.Storage.Engines.LocalStorage.Service', [])
+
+/**
+ * Local storage engine service
+ */
+.factory('LocalStorage', function LocalStorage($window) {
+
+	/**
+	 * Storage engine interface
+	 */
+	return {
+
+    /**
+     * Check if supported
+     */
+    isSupported: function() {
+
+      //Prepare test key
+      var key = '___' + Math.round(Math.random() * 1e7);
+
+      //Try local storage
+      try {
+        $window.localStorage.setItem(key, '');
+        $window.localStorage.removeItem(key);
+        return true;
+      }
+      catch(e) {
+        return false;
+      }
+    },
+
+    /**
+     * Set an item
+     */
+    set: function(key, value) {
+      $window.localStorage.setItem(key, value);
+    },
+
+    /**
+     * Get an item
+     */
+    get: function(key) {
+      return $window.localStorage.getItem(key);
+    },
+
+    /**
+     * Remove an item
+     */
+    remove: function(key) {
+      $window.localStorage.removeItem(key);
+    },
+
+		/**
+		 * Clear items
+		 */
+		clear: function(prefix) {
+
+			//Get regex for prefix and keys
+			var prefixRegex = prefix ? new RegExp('^' + prefix) : null,
+					keys = Object.keys($window.localStorage);
+
+			//Loop keys
+			for (var k = 0; k < keys.length; k++) {
+				if (!prefix || prefixRegex.test(keys[k])) {
+					$window.localStorage.removeItem(keys[k]);
+				}
+			}
+		}
+  };
+});
